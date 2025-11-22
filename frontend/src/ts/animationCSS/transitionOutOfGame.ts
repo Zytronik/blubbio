f84c09cd9adf1bb9704d96db5914b607afcf8a6e
Zytronik <omar.oberholzer@gmail.com>
2025-11-22 13:03:44 +0100

    transition out of game animation temp

---
+++ frontend/src/ts/animationCSS/transitionOutOfGame.ts
@@ -0,0 +1,49 @@
+import { hideTopAndBottomBars, setPage } from '../page/pageManager';
+import { useSoundStore } from '@/stores/soundStore';
+import gsap from 'gsap';
+import { PAGE } from '../_enum/page';
+import { useGameStore } from '@/stores/gameStore';
+import { GAME_MODE } from '../_enum/gameMode';
+import { useContainerStore } from '@/stores/containerStore';
+
+export function transitionOutOfGame(gameMode: GAME_MODE) {
+    useSoundStore().playSound('menu_back');
+    const gameStore = useGameStore();
+    const main = document.querySelector('main') as HTMLElement;
+    const overlay = document.createElement('div');
+
+    const tl = gsap.timeline();
+
+    tl.to('.pageWrapper', { duration: 0.15, x: '100vw' });
+
+    tl.set('.pageWrapper', { x: '0vw' });
+
+    tl.call(() => {
+        overlay.classList.add('gameTransitionOverlay');
+        overlay.style.cssText = `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background:black;
+    opacity: 1;
+    z-index:9999;
+  `;
+        main.appendChild(overlay);
+        hideTopAndBottomBars();
+
+        if (gameMode === GAME_MODE.SPRINT) {
+            setPage(PAGE.sprintPage);
+        } else if (gameMode === GAME_MODE.MULTI_PLAYER) {
+            setPage(PAGE.roomPage);
+        }
+
+        useContainerStore().showGame()
+        gsap.fromTo(overlay, { opacity: 1 }, { duration: 1, opacity: 0, delay: 0.5 });
+        setTimeout(() => {
+            main.removeChild(overlay);
+            gameStore.startGame();
+        }, 1500);
+    });
+}
